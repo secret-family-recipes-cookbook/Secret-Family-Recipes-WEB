cb02963dbedd08d3c06f9a224d85554c87fc8412
anthony <anthony24.kwhomes@gmail.com>
2019-07-08 13:17:02 -0700

    Added token, login/register, and restricted middleware.

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const secrets = require('../secrets/secrets.js')
+
+module.exports = (req, res, next) => {
+const token = req.headers.authorization;
+if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+        if (err) {
+            res.status(401).json({ message: 'Invalid request' })
+        }else{
+            req.decodedJwt = decodedToken;
+            next()
+        }
+    })
+}else{
+    res.status(401).json({ message: 'Invalid credentials' })
+}
+}
