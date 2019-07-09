8b18ddc86e45b75bbee3a8f665e4b10c517f90a7
anthony <anthony24.kwhomes@gmail.com>
2019-07-09 14:17:06 -0700

    env file added.

--- Backend/index.js
+++ Backend/index.js
@@ -5,5 +5,5 @@ const server = require('./api/server.js')
 const port = process.env.PORT || 2400
 
 server.listen(port, () => {
-    console.log(`\n*** Server up on port ${port} ***\n`)
+    console.log(`\n*** Server up on http://localhost:${port} ***\n`)
 });
