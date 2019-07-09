b17d2d3c711db52e63558f661ee370299ba99e3c
anthony <anthony24.kwhomes@gmail.com>
2019-07-08 17:00:45 -0700

    Added users model and router.

---
+++ users/users-model.js
@@ -0,0 +1,35 @@
+const db = require('../data/dbConfig.js')
+
+module.exports = {
+    addUser,
+    find,
+    findBy,
+    findById,
+    remove
+  }
+  
+  function find() {
+    return db('users').select('id', 'username', 'password')
+  }
+  
+  function findBy(filter) {
+    return db('users').where(filter)
+  }
+  
+  function findById(id) {
+    return db('users')
+      .select('id', 'username')
+      .where({ id })
+      .first()
+  }
+  
+  async function addUser(user) {
+    const [id] = await db('users').insert(user)
+    return findById(id)
+  }
+  
+  function remove(id) {
+    return db('users')
+      .where({ id })
+      .del()
+  }
